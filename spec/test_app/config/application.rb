af0ffa62915ab2d5a2bb972adde5ff09c9e336bf
Peter Karman <peter.karman@gsa.gov>
2016-08-17 16:08:02 -0500

    Add tests for full page in test app

--- spec/test_app/config/application.rb
+++ spec/test_app/config/application.rb
@@ -1,7 +1,8 @@
 require_relative 'boot'
 
-require "action_mailer/railtie"
 require "sprockets/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
 
 Bundler.require(*Rails.groups)
 require "uswds/rails"
@@ -11,6 +12,10 @@ module TestApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.to_prepare do
+      STDERR.puts "Loaded Rails #{Rails::VERSION::STRING}, Sprockets #{Sprockets::VERSION}",
+                  "Asset paths: #{Rails.application.config.assets.paths}"
+    end
   end
 end
 
